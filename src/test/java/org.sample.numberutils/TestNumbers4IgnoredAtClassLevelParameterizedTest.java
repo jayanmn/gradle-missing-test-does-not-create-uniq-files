5675e9c3b07d3499e7b986affff05201d264aab6
Jayanarayanan MN <jayanarayanan_mn@mentor.com>
2021-01-15 18:05:40 +0530

    save point

---
+++ src/test/java/org.sample.numberutils/TestNumbers4IgnoredAtClassLevelParameterizedTest.java
@@ -0,0 +1,32 @@
+package org.sample.numberutils;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
+@RunWith(Parameterized.class)
+public class TestNumbers4IgnoredAtClassLevelParameterizedTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"data1",},
+                {"data2"},
+
+        });
+    }
+
+    private String data;
+
+    @Test
+    @Ignore
+    public void ignoredInJunit4() {
+        System.out.println(data);
+    }
+}
