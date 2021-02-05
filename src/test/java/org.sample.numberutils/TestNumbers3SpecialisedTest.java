cd4e1d7cd2eecc2447e0c1534d0e38ad0022fd24
Jayanarayanan MN <jayanarayanan_mn@mentor.com>
2021-02-05 15:31:14 +0530

    test case junit3

--- src/test/java/org.sample.numberutils/TestNumbers3SpecialisedTest.java
+++ src/test/java/org.sample.numberutils/TestNumbers3SpecialisedTest.java
@@ -1,12 +1,10 @@
 package org.sample.numberutils;
 
 
-import junit.framework.TestCase;
-
-
-public class TestNumbers3SpecialisedTest extends TestNumbers3GeneralTestcase {
+ public class TestNumbers3SpecialisedTest extends TestNumbers3GeneralTestcase {
 
     public void testJunit3Special(){
-        System.out.println(new Numbers().add(1,2));
+        System.out.println(this.getClass().getName());
+        System.out.println(new Numbers().add(100,2));
     }
 }
